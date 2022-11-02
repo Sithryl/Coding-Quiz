19f3af534f567070a79ca35e87e9e896e4713730
David McWhirter <david@tgsummit.com>
2022-11-01 22:22:11 -0400

    added javascript outline

---
+++ script.js
@@ -0,0 +1,76 @@
+var questions = [
+    {
+        question: 'What is a header element',
+        answers: ['<h1>', '<p>', '<img>', '<footer>'],
+        answer: '<h1>',
+    },
+
+    {
+        question: 'What is a header element',
+        answers: ['<h1>', '<p>', '<img>', '<footer>'],
+        answer: '<h1>',
+    },
+
+    {
+        question: 'What is a header element',
+        answers: ['<h1>', '<p>', '<img>', '<footer>'],
+        answer: '<h1>',
+    },
+
+    {
+        question: 'What is a header element',
+        answers: ['<h1>', '<p>', '<img>', '<footer>'],
+        answer: '<h1>',
+    },
+
+    {
+        question: 'What is a header element',
+        answers: ['<h1>', '<p>', '<img>', '<footer>'],
+        answer: '<h1>',
+    },
+];
+
+var 
+
+// when i hit a button, the quiz starts
+function startGame() {
+    // hide main start screen
+    // start timer
+    // get question
+};
+
+addEventListener('click', 'start-button') {
+    startGame();
+};
+
+// a timer starts when i am presented with a question
+function timer() {
+
+};
+
+// when i answer a question i get another one
+function getQuestions() {
+// get question object at index 0 - questions[0]
+// 
+// for loop to get choices
+    // create btnEl
+// take btnEl set text to choice[i]
+};
+
+function questionClick() {
+    // event.target
+    // if correct index++
+    // if choice is not correct - time-- index++
+}
+
+// when i answer all question or if the timer is 0 the game ends
+// when the game ends i can save my score
+function endGame() {
+    // clear page of quiz
+    // get values of user initials + score
+    // set to local storage
+};
+
+function scoreboard() {
+// render scoreboard from local storage
+}
